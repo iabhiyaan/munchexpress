a690758f7510b503d5fa46eef3c1819abc982c3b
iabhiyaan <abhiyaan@yahoo.com>
2020-03-11 13:32:07 -0700

    All Files added

---
+++ resources/js/components/OrderItems.vue
@@ -0,0 +1,27 @@
+<template>
+  <tbody>
+    <tr v-for="order in orders" :key="order.id">
+      <td>{{order.id}}</td>
+      <td>{{order.amount}}</td>
+      <td>{{(order.isComplete) ? 'Completed' : 'Incomplete'}}</td>
+      <td>
+        Name: {{order.order_details.customer_name}}
+        <br>
+        Phone: {{order.order_details.customer_phone}}
+        <br>
+        Address: {{order.order_details.customer_address}}
+      </td>
+      <td>
+        <!-- icons taken from https://coolsymbol.com -->
+        <button class="btn btn-success mr-3">✔</button>
+        <button class="btn btn-warning">✘</button>
+      </td>
+    </tr>
+  </tbody>
+</template>
+
+<script>
+export default {
+  props: ["orders"]
+};
+</script>
